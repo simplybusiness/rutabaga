8baff2a13430a134fb730ed24001482065cdb2df
Lukas Oberhuber <lukaso@gmail.com>
2012-08-24 17:31:39 +0100

    Cleanup

---
+++ lib/rutabaga/rspec_scary_speed_fix.rb
@@ -0,0 +1,11 @@
+# speed up rspec under ruby 1.9 because it doesn't work otherwise
+if RUBY_VERSION > '1.9.2'
+  class Object
+    def to_ary ; nil ; end
+    def to_hash ; nil ; end
+  end
+
+  class String
+    def to_path ; self ; end
+  end
+end
