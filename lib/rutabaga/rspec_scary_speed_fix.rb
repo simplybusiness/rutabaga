7a74b351fa330daf0f0a02dd047901e0ad84d388
Lukas Oberhuber <lukaso@gmail.com>
2012-08-26 20:28:29 +0100

    Make scary fix MRI only.

--- lib/rutabaga/rspec_scary_speed_fix.rb
+++ lib/rutabaga/rspec_scary_speed_fix.rb
@@ -1,5 +1,5 @@
 # speed up rspec under ruby 1.9 because it doesn't work otherwise
-if RUBY_VERSION > '1.9.2'
+if RUBY_VERSION > '1.9.2' and RUBY_ENGINE == 'ruby'
   class Object
     def to_ary ; nil ; end
     def to_hash ; nil ; end
