5042fd0096cbbeda584a017d740e5390a4dd8079
Daniel Barlow <daniel.barlow@simplybusiness.co.uk>
2013-09-23 14:44:31 +0100

    add a feature for the new feature feature

---
+++ examples/test3.feature
@@ -0,0 +1,5 @@
+Feature: Test that rspec will call the feature by filename
+
+Scenario: ensures the feature is called
+Given that 2 * 4 is calculated
+Then my result is 8
--- examples/test_spec.rb
+++ examples/test_spec.rb
@@ -46,3 +46,19 @@ describe "causes a failing test" do
   end
 
 end
+
+describe "finds a feature file given as parameter to the 'feature' method" do
+  it "implements the named feature" do
+    feature "examples/test3.feature"
+  end
+
+  step "that :first * :second is calculated" do |first, second|
+    @first = first
+    @second = second
+  end
+
+  step "my result is :result" do |result|
+    result.to_i.should == @first.to_i * @second.to_i
+  end
+
+end
