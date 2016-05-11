ae998e4f6a9bf1f979b43d4b8e1e82cc4c43da80
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2016-05-11 10:37:02 +0100

    Refactor slightly

--- lib/rutabaga/feature.rb
+++ lib/rutabaga/feature.rb
@@ -3,8 +3,8 @@ require 'rspec'
 
 module Rutabaga
   module Feature
-    def feature(feature_file = find_feature)
-
+    def feature(feature_file = nil)
+      feature_file = Util.find_feature(feature_file || RSpec.current_example.description)
       example_group_class = self.class
 
       # Hack turnip into the rspec only when needed
@@ -12,13 +12,6 @@ module Rutabaga
       example_group_class.send(:include, Turnip::Steps)
 
       Turnip::RSpec.rutabaga_run(feature_file, example_group_class)
-    end
-
-    private
-
-    def find_feature
-      description = RSpec.current_example.description
-      Util.find_feature(description)
     end
   end
 end
