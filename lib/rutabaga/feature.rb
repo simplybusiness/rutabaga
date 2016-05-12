54dc959f6423f0596706a25a2e96326b032fa537
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2016-05-12 11:34:43 +0100

    Revert simplification
    Steps need to be registered to the surrounding example group.

--- lib/rutabaga/feature.rb
+++ lib/rutabaga/feature.rb
@@ -18,7 +18,36 @@ module Rutabaga
       example_group_class.send(:include, Turnip::RSpec::Execute)
       example_group_class.send(:include, Turnip::Steps)
 
-      Turnip::RSpec.rutabaga_run(feature_file, example_group_class)
+      run(feature_file, example_group_class)
+    end
+
+    # Adapted from jnicklas/turnip v2.0.0
+    def run(feature_file, example_group_class)
+      Turnip::Builder.build(feature_file).features.each do |feature|
+        describe = example_group_class.describe feature.name, feature.metadata_hash
+        run_feature(describe, feature, feature_file, example_group_class)
+      end
+    end
+
+    def run_feature(describe, feature, filename, example_group_class)
+      example_group_class.before do
+        # This is kind of a hack, but it will make RSpec throw way nicer exceptions
+        RSpec.current_example.metadata[:file_path] = filename
+
+        feature.backgrounds.map(&:steps).flatten.each do |step|
+          run_step(filename, step)
+        end
+      end
+
+      feature.scenarios.each do |scenario|
+        example_group_class.describe scenario.name, scenario.metadata_hash do
+          it(scenario.steps.map(&:to_s).join(' -> ')) do
+            scenario.steps.each do |step|
+              run_step(filename, step)
+            end
+          end
+        end
+      end
     end
   end
 end
