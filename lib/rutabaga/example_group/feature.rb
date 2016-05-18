a5c7d4a7c8dc3521631f80ba60a54e73901f4477
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2016-05-18 13:13:35 +0100

    Simplify rspec monekey patching

--- lib/rutabaga/example_group/feature.rb
+++ lib/rutabaga/example_group/feature.rb
@@ -13,45 +13,21 @@ end
 
 # Monkey patch RSpec to add the feature method in example groups
 class RSpec::Core::ExampleGroup
-  def self.define_feature_method(metadata={})
-    idempotently_define_singleton_method(:feature) do |*args, &example_group_block|
-      thread_data = RSpec::Support.thread_local_data
-      top_level   = self == RSpec::Core::ExampleGroup
+  class << self
+    alias_method :orig_subclass, :subclass
 
-      if top_level
-        if thread_data[:in_example_group]
-          raise "Creating an isolated context from within a context is " \
-                "not allowed. Change `RSpec.#{name}` to `#{name}` or " \
-                "move this to a top-level scope."
+    def subclass(parent, description, args, &example_group_block)
+      self.orig_subclass(parent, description, args, &example_group_block).tap do |describe|
+
+        if args.any? { |arg| arg.kind_of?(Hash) && arg[:rutabaga] }
+          Rutabaga::ExampleGroup::Feature.feature(describe, description, args)
         end
 
-        thread_data[:in_example_group] = true
-      end
-
-      begin
-
-        description = args.shift
-        combined_metadata = metadata.dup
-        combined_metadata.merge!(args.pop) if args.last.is_a? Hash
-        args << combined_metadata
-
-        feature_class = subclass(self, description, args, &example_group_block).tap do |child|
-          children << child
-        end
-
-        Rutabaga::ExampleGroup::Feature.feature(feature_class, description, args)
-
-        feature_class
-
-      ensure
-        thread_data.delete(:in_example_group) if top_level
       end
     end
-
-    RSpec::Core::DSL.expose_example_group_alias(:feature)
   end
 
-  define_feature_method
+  define_example_group_method :feature, :rutabaga => true
 end
 
 module Rutabaga
